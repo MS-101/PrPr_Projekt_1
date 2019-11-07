e1d83b1cc9305f9bd9c58372280b7d5a7a042a97
Martin Svab <martin.svab@gmail.com>
2019-11-07 13:11:57 +0100

    Pridany smernik na subor a vylepsena funkcia koniec() a vypis()

--- Projekt.c
+++ Projekt.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 
-int vypis() {
-    FILE *fr;
-    if ((fr = fopen("autobazar.txt", "r")) == NULL) {
-        printf("Neotvoreny subor\n");
-        return 1;
+int vypis(FILE **fr) {
+    if (*fr == NULL) {
+        if ((*fr = fopen("autobazar.txt", "r")) == NULL) {
+            printf("Neotvoreny subor\n");
+            return 1;
+        }
+    } else {
+        rewind(*fr);
     }
-
-    char str[100];
+    //str obsahuje 51 znakov, lebo 51. znak je '/0' ak do neho naèítam záznam s 50 znakmi.
+    char str[51];
     double num;
-    while(fgets(str, 100, fr) > 1) {
-        printf("meno a priezvisko: %s", str);
-        fgets(str, 100, fr);
-        printf("SPZ: %s", str);
-        fgets(str, 100, fr);
-        printf("typ auta: %s", str);
-        fscanf(fr, "%lf\n", &num);
+    while(fgets(str, 51, *fr) > 1) {
+        if (str[strlen(str) - 1] == '\n') {
+            str[strlen(str) - 1] = '\0';
+        }
+        printf("meno a priezvisko: %s\n", str);
+        fgets(str, 8, *fr);
+        fscanf(*fr, "\n");
+        printf("SPZ: %s\n", str);
+        fgets(str, 2, *fr);
+        printf("typ auta: %s\n", str);
+        fscanf(*fr, "%lf\n", &num);
         printf("cena: %.2f\n", num);
-        fgets(str, 100, fr);
-        printf("datum: %s", str);
+        fgets(str, 9, *fr);
+        printf("datum: %d\n", str);
         printf("\n");
-        fscanf(fr, "\n");
+        fscanf(*fr, "\n");
     }
-
-    fclose(fr);
-
     return 0;
 }
 
@@ -48,15 +52,25 @@ void palindrom() {
 
 }
 
+int koniec(FILE **fr) {
+    if (*fr != NULL) {
+        if (fclose(*fr) == EOF) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 void zistiPredaj() {
 
 }
 
 int main() {
+    FILE *fr = NULL;
     while (1)  {
         switch (getchar()) {
             case 'v':
-                vypis();
+                vypis(&fr);
                 break;
             case 'o':
                 odmena();
@@ -77,7 +91,7 @@ int main() {
                 zistiPredaj();
                 break;
             case 'k':
-                return 0;
+                return koniec(&fr);
                 break;
         }
     }
